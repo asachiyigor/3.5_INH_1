d075a0776ecad8a72bdd702e7c2d5176d7a3dc78
asachiyigor <asachiyigor@gmail.com>
2022-04-11 22:07:18 +0300

    3.5.1.4 EXC 1-2 done

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -10,15 +10,26 @@ public class Repository {
     }
 
     public void save(Product item) {
+
+        for (Product product : product) {
+            if (product.getId() == item.getId()) {
+                throw new AlreadyExistsException("Element with id: " + item.getId() + " already exists");
+            }
+        }
         int length = product.length + 1;
         Product[] tmp = new Product[length];
         System.arraycopy(product, 0, tmp, 0, product.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         product = tmp;
+
     }
 
     public void removeById(int id) {
+
+        if (findById(id) == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
+        }
         int length = product.length - 1;
         Product[] tmp = new Product[length];
         int index = 0;
@@ -29,5 +40,14 @@ public class Repository {
             }
         }
         product = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : product) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
     }
 }
--- src/test/java/ru/netology/repository/RepositoryTest.java
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -13,9 +13,11 @@ class RepositoryTest {
     private Product first = new Product(1, "Pen", 1000);
     private Book second = new Book(2, "Alice in Wonderland", 500, "Lewis Carroll");
     private Smartphone third = new Smartphone(3, "8800 Sirocco Edition", 10000, "Nokia");
+    private Smartphone fourth = new Smartphone(3, "6610", 10000, "Nokia");
 
     @Test
-    void shouldRemoveById() {
+        // Задание 1. NotFoundException (обязательное к выполнению)
+    void shouldRemoveById() { // первый должен проверять успешность удаления существующего элемента
         repository.save(first);
         repository.save(second);
         repository.save(third);
@@ -26,5 +28,44 @@ class RepositoryTest {
         Product[] expected = {first, third};
 
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+        // Задание 1. NotFoundException (обязательное к выполнению)
+    void shouldGenerateNotFoundException() { // второй - генерации NotFoundException при попытке удаления несуществующего элемента
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        repository.removeById(2);
+
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(2);
+        });
+    }
+
+    @Test
+        //Задание 2*. AlreadyExistsException (НЕобязательная задача)
+    void shouldAddNewProduct() { // первый должен проверять успешность добавления элемента
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {first, second, third};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+        //Задание 2*. AlreadyExistsException (НЕобязательная задача)
+    void shouldGenerateAlreadyExistsException() { // второй - генерации AlreadyExistsException при попытке добавить элемент с повторяющимся id.
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        assertThrows(AlreadyExistsException.class, () -> {
+            repository.save(fourth);
+        });
     }
 }
